3db24c5e88273b42f452a152aa8a82d29da5d6e4
R210532 <159106565+R210532@users.noreply.github.com>
2024-05-05 20:40:09 +0530

    Add files via upload

---
+++ division.m
@@ -0,0 +1,10 @@
+a=input("enter a first number:");
+b=input("enter a second number:");
+try
+    division=a/b
+    disp(['result of division';num 2 str(division)])
+catch
+    disp("error :enter either float or integer value")
+end_try_catch
+
+
