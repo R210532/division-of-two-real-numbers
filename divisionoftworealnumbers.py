5c1ec625cc0d72d1a11ab1450a365fdf677918dd
R210532 <159106565+R210532@users.noreply.github.com>
2024-05-05 20:39:40 +0530

    Add files via upload

---
+++ divisionoftworealnumbers.py
@@ -0,0 +1,12 @@
+#division of two real numbers
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+if ((type(a) == "int" or "float") and (type(b)=="int" or "float")):
+
+    print(a/b)
+else:
+    print("The number are not real")
+
+
+    
+
